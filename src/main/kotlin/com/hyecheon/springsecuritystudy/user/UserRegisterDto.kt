1f08ee63140364759cd2933b6a217bb164c12507
이혜천 <rainbow880616@gmail.com>
2021-09-14 09:22:49 +0900

    #5 - 도메인 추가
    서비스 , 리포지토리 , 컨트롤러 도메인 익셉션 추가

---
+++ src/main/kotlin/com/hyecheon/springsecuritystudy/user/UserRegisterDto.kt
@@ -0,0 +1,11 @@
+package com.hyecheon.springsecuritystudy.user
+
+/**
+ * User: hyecheon lee
+ * Email: rainbow880616@gmail.com
+ * Date: 2021/09/14
+ */
+data class UserRegisterDto(
+    val username: String,
+    val password: String,
+)
